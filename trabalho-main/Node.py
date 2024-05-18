c3d472ee820ab2604f54d0e55bab58380c0976e4
coompaadree <147946141+coompaadree@users.noreply.github.com>
2024-05-18 19:14:00 +0100

    Versão mais atualizada!
    Fiz assim para não estragar o antigo.
    Falta principalmente:
    - Novos e variados testSets. (~1 basico e sem exceções, 4 cada um relacionado com exceções expecificas, 1 geral e muito abrangente);
    
     - O Eterno problema da lista não poder ser maior do que três. Eu tentei resolver, mas a framework de resolução pela qual optei só me trouxe chatices e resólvir que o melhor é fazer do zero e pensar noutra forma de resolver o problema. Caso venha a conseguir, adicionarei aqui.

---
+++ trabalho-main/Node.py
@@ -0,0 +1,68 @@
+# 2023-2024 Programacao 2 LTI
+# Grupo 3
+# 58000 Ana Rita Nicolau
+# 62224 André Alexandre
+
+class Node(object):
+    """
+    Class of Nodes
+    """
+    
+    def __init__(self, name):
+        """
+        Constructs a Node
+        
+        Requires:
+        name is a string
+        Ensures:
+        node such that name == self.getName()
+        """
+        self._name = name
+
+        
+    def getName(self):
+        """
+        Gets the name
+        """
+        return self._name
+
+
+    def setName(self,newName):
+        """
+        Sets self._name new value.
+
+        Requires:
+        newName is str
+        Ensures:
+        self.getName() == newName
+        """
+        self._name = newName
+
+
+    def __eq__ (self, otherNode):
+        """
+        Compares two Node objects attributes.
+        
+        Requires: otherNode is Node object
+        Ensures: A boolean value obtained from the equality comparison of
+        _name attribute of both Node objects
+        """
+        return self.getName()==otherNode.getName()
+
+
+    def __lt__(self, otherNode):
+        """
+        Compares two Node objects attributes.
+        
+        Requires: otherNode is Node object
+        Ensures: A boolean value obtained from the less than comparison
+        of _name attribute of both Node objects by node's name
+        """
+        return self.getName()<otherNode.getName()
+    
+
+    def __str__(self):
+        """
+        String representation
+        """
+        return self._name
